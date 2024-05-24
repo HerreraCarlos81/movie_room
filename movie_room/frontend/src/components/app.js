aeeb15d9b3584d00ca3da736bed2d96282a38e45
Carlos Eduardo Vargas Herrera <cevh.81@outlook.com>
2024-05-24 02:03:49 +0000

    Project under review for adaptation to Movie Rooms powered by Hyperbeam

--- movie_room/frontend/src/components/app.js
+++ movie_room/frontend/src/components/app.js
@@ -1,15 +1,25 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import HomePage from "./components/Homepage";
+import RoomJoinPage from "./components/RoomJoinPage";
+import CreateRoomPage from "./components/CreateRoomPage";
 
 export default class App extends Component {
     constructor(props) {
         super(props);
+        
     }
 
     render() {
-        return <h1>Testing React Code</h1>
+        return (
+            <div>
+                <HomePage />
+                <RoomJoinPage />
+                <CreateRoomPage />
+            </div>
+        )
     }
 }
 
 const appDiv = document.getElementById("app");
-render(<App />, appDiv)
+render(<App name="Jess" />, appDiv)
